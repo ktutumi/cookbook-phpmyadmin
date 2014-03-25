d1dfcd8c667b990741de2209504c62dc358df35d
Koichi Tsutsumi <koichi.tsutsumi@gmail.com>
2014-03-25 09:09:59 +0900

    Update metadata.rb
    0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Koichi Tsutsumi <koichi.tsutsumi@gmail.com>'
 license          'All rights reserved'
 description      'Installs/Configures phpMyAdmin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'apache2'
 depends 'tar'
